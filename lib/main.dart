966549461c93aa8de3bcf3fa303e6528bc486bf3
dhane31 <dhanegm731@gmail.com>
2021-04-04 07:37:51 +0900

    Add firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
@@ -8,10 +8,10 @@ import 'package:flutter_signin_button/flutter_signin_button.dart';
 final googleSignInProvider =
     ChangeNotifierProvider((ref) => GoogleSignInProvider());
 
-Future main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -31,36 +31,110 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Consumer(builder: (context, watch, child) {
+      final provider = watch(googleSignInProvider);
+
+      return Scaffold(
         appBar: AppBar(
           title: Text('Google Sign In'),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SignInButton(
-                Buttons.Google,
-                text: "Sign up with Google",
-                onPressed: () {},
-              ),
-            ],
-          ),
-        ));
+        body: StreamBuilder(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (context, snapshot) {
+              if (provider.isSignIn) {
+                return Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasData) {
+                final user = FirebaseAuth.instance.currentUser;
+                return Container(
+                  alignment: Alignment.center,
+                  color: Colors.blueGrey[700],
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text('Logged In', style: TextStyle(color: Colors.white)),
+                      SizedBox(height: 10),
+                      CircleAvatar(
+                          maxRadius: 25,
+                          backgroundImage: NetworkImage(user.photoURL)),
+                      SizedBox(height: 10),
+                      Text('Name: ' + user.displayName,
+                          style: TextStyle(color: Colors.white)),
+                      SizedBox(height: 10),
+                      Text('Email: ' + user.email,
+                          style: TextStyle(color: Colors.white)),
+                      SizedBox(height: 10),
+                      ElevatedButton(
+                        onPressed: () {
+                          context.read(googleSignInProvider).logout();
+                        },
+                        child: Text('Logout'),
+                      ),
+                    ],
+                  ),
+                );
+              } else {
+                return Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SignInButton(
+                        Buttons.Google,
+                        text: "Sign up with Google",
+                        onPressed: () {
+                          context.read(googleSignInProvider).login();
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              }
+            }),
+      );
+    });
   }
 }
 
 class GoogleSignInProvider extends ChangeNotifier {
   final googleSignIn = GoogleSignIn();
-  bool _isSignIn = false;
+  bool _isSignIn;
+
+  GoogleSignInProvider() {
+    _isSignIn = false;
+  }
 
   bool get isSignIn => _isSignIn;
 
   set isSignIn(bool isSignIn) {
     _isSignIn = isSignIn;
+    notifyListeners();
   }
 
-  Future login() async {}
+  //ログイン
+  Future login() async {
+    isSignIn = true;
+    try {
+      final user = await googleSignIn.signIn();
+
+      if (user == null) {
+        isSignIn = false;
+        return;
+      } else {
+        final auth = await user.authentication;
+        final credential = GoogleAuthProvider.credential(
+          accessToken: auth.accessToken,
+          idToken: auth.idToken,
+        );
+        await FirebaseAuth.instance.signInWithCredential(credential);
+
+        isSignIn = false;
+      }
+    } on FirebaseAuthException catch (e) {
+      print('FirebaseAuthException: ${e.code}');
+    } on Exception catch (e) {
+      print('Exception: ${e.toString()}');
+    }
+  }
 
   //ログアウト
   void logout() async {
